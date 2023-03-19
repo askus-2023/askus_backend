b4763da65d55f52936fbbbc34319d615d4b9ca9a
hgene0929 <hgene0929@gmail.com>
2023-03-19 23:05:48 +0900

    [feat] initial commit

---
+++ src/main/java/com/askus/askus/AskusApplication.java
@@ -0,0 +1,13 @@
+package com.askus.askus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/askus/askus/AskusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.askus.askus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
