def9e6419478773af365da49ad942e0170c22827
hgene0929@gmail.com <hgene0929@gmail.com>
2023-04-10 13:14:28 +0900

    [test] 게시글 좋아요 추가/삭제 기능 테스트 & 오류 수정

--- src/test/java/com/askus/askus/domain/like/service/LikeServiceImplTest.java
+++ src/test/java/com/askus/askus/domain/like/service/LikeServiceImplTest.java
@@ -1,14 +1,83 @@
 package com.askus.askus.domain.like.service;
 
+import static org.assertj.core.api.AssertionsForClassTypes.*;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.askus.askus.domain.board.domain.Board;
+import com.askus.askus.domain.board.domain.Category;
+import com.askus.askus.domain.board.repository.BoardRepository;
+import com.askus.askus.domain.like.dto.LikeAddAndDeleteRequest;
+import com.askus.askus.domain.like.dto.LikeAddAndDeleteResponse;
+import com.askus.askus.domain.users.domain.Users;
+import com.askus.askus.domain.users.repository.UsersRepository;
+import com.askus.askus.global.config.TestConfig;
+
+@Import({TestConfig.class})
 @SpringBootTest
 @Transactional
 class LikeServiceImplTest {
 	@Autowired
 	private LikeService likeService;
+	@Autowired
+	private UsersRepository usersRepository;
+	@Autowired
+	private BoardRepository boardRepository;
 
+	@Test
+	void 좋아요_등록_성공() {
+		// given
+		Users users = new Users("email", "password", "nickname");
+		Users savedUsers = usersRepository.save(users);
 
+		Board board = new Board(
+			savedUsers,
+			"title",
+			Category.KOREAN,
+			"ingredients",
+			"content",
+			"tag"
+		);
+		Board savedBoard = boardRepository.save(board);
+
+		LikeAddAndDeleteRequest request = new LikeAddAndDeleteRequest(savedBoard.getId());
+
+		// when
+		LikeAddAndDeleteResponse response = likeService.addLike(savedUsers.getId(), request);
+
+		// then
+		assertThat(response.getBoardId()).isEqualTo(savedBoard.getId());
+		assertThat(response.getLikeCount()).isEqualTo(1L);
+	}
+
+	@Test
+	void 좋아요_삭제_성공() {
+		// given
+		Users users = new Users("email", "password", "nickname");
+		Users savedUsers = usersRepository.save(users);
+
+		Board board = new Board(
+			savedUsers,
+			"title",
+			Category.KOREAN,
+			"ingredients",
+			"content",
+			"tag"
+		);
+		Board savedBoard = boardRepository.save(board);
+
+		LikeAddAndDeleteRequest request = new LikeAddAndDeleteRequest(savedBoard.getId());
+		likeService.addLike(savedUsers.getId(), request);
+
+		// when
+		LikeAddAndDeleteResponse response = likeService.deleteLike(savedUsers.getId(), request);
+
+		// then
+		assertThat(response.getBoardId()).isEqualTo(savedBoard.getId());
+		assertThat(response.getLikeCount()).isEqualTo(0L);
+	}
 }
