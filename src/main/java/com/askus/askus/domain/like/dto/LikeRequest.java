f5322f1ae6e30ab277cf728e8168b489aa1268bf
hgene0929@gmail.com <hgene0929@gmail.com>
2023-04-24 18:49:12 +0900

    [refactor] 좋아요 추가/삭제 api 리팩토링

--- src/main/java/com/askus/askus/domain/like/dto/LikeRequest.java
+++ src/main/java/com/askus/askus/domain/like/dto/LikeRequest.java
@@ -4,15 +4,15 @@ import com.askus.askus.domain.board.domain.Board;
 import com.askus.askus.domain.like.domain.Like;
 import com.askus.askus.domain.users.domain.Users;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class LikeRequest {
+	@Schema(description = "board ID", example = "1")
 	private long boardId;
-
-	public LikeRequest(long boardId) {
-		this.boardId = boardId;
-	}
 
 	public Like toEntity(Users users, Board board) {
 		return new Like(users, board);
--- src/main/java/com/askus/askus/domain/like/dto/LikeResponse.java
+++ src/main/java/com/askus/askus/domain/like/dto/LikeResponse.java
@@ -2,11 +2,14 @@ package com.askus.askus.domain.like.dto;
 
 import com.askus.askus.domain.board.domain.Board;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class LikeResponse {
+	@Schema(description = "board ID", example = "1")
 	private final long boardId;
+	@Schema(description = "좋아요 수", example = "36")
 	private final long likeCount;
 
 	public LikeResponse(long boardId, long likeCount) {
